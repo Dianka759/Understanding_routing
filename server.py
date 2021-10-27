f8add5cb696edd70ad415541e659709600e24895
dianka759 <dianka759@gmail.com>
2021-10-27 02:24:34 -0400

    first commit

--- server.py
+++ server.py
@@ -1,25 +1,25 @@
-from flask import Flask  # Import Flask to allow us to create our app
-app = Flask(__name__)    # Create a new instance of the Flask class called "app"
+from flask import Flask
+app = Flask(__name__)
 
-@app.route('/')          # The "@" decorator associates this route with the function immediately following
+@app.route('/')
 def hello_world():
-    return 'Hello World!'  # Return the string 'Hello World!' as a response
+    return 'Hello World!'
 
 @app.route('/dojo')
 def dojo():
     return "Dojo!"
 
-@app.route('/say/<string:name>') # for a route '/hello/____' anything after '/hello/' gets passed as a variable 'name'
+@app.route('/say/<string:name>') 
 def hi(name):
-    return f"Hi {name}!"
+    return f"Hi {name.capitalize()}!"
 
-@app.route('/repeat/<int:num>/<string:name>') # for a route '/users/____/____', two parameters in the url get passed as username and id
-def srepeat(num, name):
-    return name * num
+@app.route('/repeat/<int:num>/<string:name>') 
+def repeat(num, name):
+    return f"{name} * {num} = {name * num}"
 
 @app.errorhandler(404) # only works for error 404, page not found 404 status explicitly
 def page_not_found(e):
     return ('Sorry! No response. Try again.'), 404
 
-if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
-    app.run(debug=True)    # Run the app in debug mode.
+if __name__=="__main__":
+    app.run(debug=True)
